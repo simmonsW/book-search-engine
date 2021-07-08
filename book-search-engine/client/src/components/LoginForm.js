e5b7baa82ebf89ce856aa55b34854101db5675f5
Wyatt Simmons <simmonsw182@gmail.com>
2021-07-08 16:55:32 -0700

    prune old API related code from LoginForm.js and SignupForm.js

--- book-search-engine/client/src/components/LoginForm.js
+++ book-search-engine/client/src/components/LoginForm.js
@@ -2,11 +2,9 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-// import { loginUser } from '../utils/API';
-import Auth from '../utils/auth';
-
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
@@ -36,16 +34,6 @@ const LoginForm = () => {
       });
 
       Auth.login(data.login.token);
-
-      // const response = await loginUser(userFormData);
-
-      // if (!response.ok) {
-      //   throw new Error('something went wrong!');
-      // }
-
-      // const { token, user } = await response.json();
-      // console.log(user);
-      // Auth.login(token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);

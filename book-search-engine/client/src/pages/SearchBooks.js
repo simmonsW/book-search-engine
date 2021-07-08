482d32229975da01c42abed8d5e188930a7b8f65
Wyatt Simmons <simmonsw182@gmail.com>
2021-07-08 16:59:34 -0700

    prune old API related code from SavedBooks.js and SearchBooks.js

--- book-search-engine/client/src/pages/SearchBooks.js
+++ book-search-engine/client/src/pages/SearchBooks.js
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
+import { useMutation } from '@apollo/client';
+import { SAVE_BOOK } from '../utils/mutations';
 import Auth from '../utils/auth';
 import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
-
-import { useMutation } from '@apollo/client';
-import { SAVE_BOOK } from '../utils/mutations';
 
 const SearchBooks = () => {
   const [saveBook] = useMutation(SAVE_BOOK);
@@ -61,20 +60,7 @@ const SearchBooks = () => {
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
 
-    // get token
-    // const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-    // if (!token) {
-    //   return false;
-    // }
-
     try {
-      // const response = await saveBook(bookToSave, token);
-
-      // if (!response.ok) {
-      //   throw new Error('something went wrong!');
-      // }
-
       await saveBook({
         variables: {
           bookToSave: {
